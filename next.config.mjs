354f2c9f27eb982e885d909c17ec0f0a64c0e19a
GreatWolf20XX <artem.naumov.dev@gmail.com>
2024-09-13 21:39:14 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+    basePath: '/CloudWave',
+    assetPrefix: '/CloudWave/',
     images: {
-        loader: "akamai",
-        path: "/",
+        unoptimized: true,
     },
 };
 
