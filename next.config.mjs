af2aca788eee3f005bd0317f9d87fcd9ac139800
GreatWolf20XX <artem.naumov.dev@gmail.com>
2024-09-13 21:47:32 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    images: {
-      loader: "akamai",
-      path: "",
-    },
-    assetPrefix: "./",
+    basePath: '/CloudWave',
+    assetPrefix: 'https://greatwolfxx.github.io'
 };
 
 export default nextConfig;
